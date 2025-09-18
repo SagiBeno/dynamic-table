4ec201f6d663ae5985385cddcba8ae5bbd15daf6
SagiBeno <benosagi321@gmail.com>
2025-09-18 10:50:54 +0200

    Store props data in state

--- react-app/src/Components/MyTable.jsx
+++ react-app/src/Components/MyTable.jsx
@@ -2,16 +2,33 @@ import React from "react";
 import './MyTable.css'
 
 export default class MyTable extends React.Component {
+    
+    state = {
+        /* Stílus így is megadható
+        style: {
+            table: {},
+            td: {}
+        }
+        */   
+       rows: [] 
+    }
+      
+
     render() {
         return <div>
             <table className="table">
                 <tbody>
                     <tr>
-                        <td>TODO</td>
-                        <td>TODO</td>
+                        <td>TODO - {JSON.stringify(this.state.rows)}</td>
                     </tr>
                 </tbody>
             </table>
         </div>
+    }
+
+    componentDidMount() {
+        const words = this.props.data.split(" ")
+        console.log('words', words)
+        this.setState({rows: words})
     }
 }
