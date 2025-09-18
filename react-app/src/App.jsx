a9cd67ea1970e6213d560ca789a0261c197a8df7
SagiBeno <benosagi321@gmail.com>
2025-09-18 10:40:21 +0200

    Style MyTable

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import MyTable from './Components/MyTable'
+import './App.css'
 
 export default class App extends React.Component {
   render() {
--- react-app/src/Components/MyTable.jsx
+++ react-app/src/Components/MyTable.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import './MyTable.css'
+
 export default class MyTable extends React.Component {
     render() {
         return <div>
-            
+            <table className="table">
+                <tbody>
+                    <tr>
+                        <td>TODO</td>
+                        <td>TODO</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     }
 }
